88cf0276e6dd0c17bf311740b61c7222689bf404
Mohamed Metwally <metwallym527@gmail.com>
2026-02-11 15:52:20 +0200

    Add files via upload

---
+++ CSharp06/CSharp06/ClassAndStruct.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp06.CSharp06
+{
+    #region Problem1
+    internal struct Point
+    {
+        int X;
+        int Y;
+
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"point X={X} and point Y={Y}";
+        }
+
+    }
+    #endregion
+
+    #region Problem2
+    //internal class TypeA
+    //{
+    //    private int F = 10;
+    //    internal int G = 20;
+    //    public int H = 30;
+
+    //    //in same class
+    //    public void show()
+    //    {
+    //        Console.WriteLine($"F={F}, G={G}, H={H}");
+    //    }
+    //}
+    #endregion
+
+    #region Problem3
+    //internal struct Employee
+    //{
+    //    private int EmpId;
+    //    private string Name;
+    //    private int Salary;
+
+    //    public void SetName(string name)
+    //    {
+    //        Name = name;
+    //    }
+
+    //    public string GetName()
+    //    {
+    //        return Name;
+    //    }
+
+    //    public int salary
+    //    {
+    //        get { return Salary; }
+    //        set { Salary = value; }
+    //    }
+
+    //    public int employeeID { get; set; }
+
+    //}
+    #endregion
+
+    #region Problem 4+5
+    //internal struct Point
+    //{
+    //    private int X;
+    //    private int Y;
+
+    //    public Point(int x)
+    //    {
+    //        X = x;
+    //        Y = 0;
+    //    }
+
+    //    public Point(int x,int y)
+    //    {
+    //        X = x;
+    //        Y = y;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return $"X={X}, Y={Y}";
+    //    } 
+    #endregion
+
+    #region Problem6
+    //internal struct Point
+    //{
+    //    public int X;
+    //    public int Y;
+
+    //    public Point(int x, int y)
+    //    {
+    //        X = x;
+    //        Y = y;
+    //    }
+    //}
+
+    //internal class Employee
+    //{
+    //    public string Name;
+    //    public double Salary;
+
+    //    public Employee(string name, double salary)
+    //    {
+    //        Name = name;
+    //        Salary = salary;
+    //    }
+    //} 
+    #endregion
+
+
+
+}
+
---
+++ CSharp06/CSharp06/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CSharp06.CSharp06
+{
+    internal class Program
+    {
+        #region Problem6
+        //public static void ModifyPoint(Point p)
+        //{
+        //    p.X += 10;
+        //    p.Y += 10;
+        //    Console.WriteLine($"X={p.X}, Y = {p.Y}");
+        //}
+
+        //public static void ModifyEmployee(Employee e)
+        //{
+        //    e.Name = "Ahmed";
+        //    e.Salary += 1000;
+        //    Console.WriteLine($"Name = {e.Name}, Salary = {e.Salary}");
+        //} 
+        #endregion
+        static void Main()
+        {
+            #region Problem1
+            Point p1 = new Point(6, 7);
+            Console.WriteLine(p1);
+            #endregion
+
+            #region Problem2
+            //TypeA T = new TypeA();
+
+            //T.show();
+
+            ////outside class typeA but inside same Project
+
+            ////Console.WriteLine(T.F);  //Error: f is private
+            //Console.WriteLine(T.G); // internal is accessible
+            //Console.WriteLine(T.H); //public is accessible 
+            #endregion
+
+            #region Problem3
+
+            //Employee em = new Employee();
+            //em.SetName("Mohamed");
+            //Console.WriteLine(em.GetName());
+
+            //em.salary = 120;
+            //Console.WriteLine(em.salary);
+
+            //em.employeeID = 10;
+            //Console.WriteLine(em.employeeID); 
+            #endregion
+
+            #region Problem 4+5
+            //Point p1 = new Point(5);
+            //Console.WriteLine(p1.ToString());
+
+            //Point p2= new Point(5,6);
+            //Console.WriteLine(p2.ToString()); 
+            #endregion
+
+            #region Problem6
+            //Point p1 = new Point(5, 5);
+            //Employee emp = new Employee("Mohamed", 5000);
+
+            //Console.WriteLine("Before modification:");
+            //Console.WriteLine($" X = {p1.X}, Y = {p1.Y}");
+            //Console.WriteLine($"Name = {emp.Name}, Salary = {emp.Salary}");
+
+            //ModifyPoint(p1);
+            //ModifyEmployee(emp);
+
+            //Console.WriteLine("\nAfter modification:");
+            //Console.WriteLine($"X = {p1.X}, Y = {p1.Y}");
+            //Console.WriteLine($"Name = {emp.Name}, Salary = {emp.Salary}");
+
+            #endregion
+
+        }
+    }
+}
