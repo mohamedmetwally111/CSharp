3124307f7af81b742209c33fec60670b4db70cc6
Mohamed Metwally <metwallym527@gmail.com>
2026-02-14 21:24:25 +0200

    Add files via upload

---
+++ CSharp07/CSharp07/Inheritance.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp07.CSharp07
+{
+    #region Problem3+4+5
+
+    #region Parent Class
+    //internal class Parent
+    //{
+    //    public int X { get; set; }
+    //    public int Y { get; set; }
+    //    public Parent(int x, int y)
+    //    {
+    //        X = x;
+    //        Y = y;
+    //    }
+
+    //    public virtual int Product()
+    //    {
+    //        return X * Y;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return $"X={X}, Y={Y}";
+    //    }
+    //}
+    #endregion
+
+    #region Child Class
+    //internal class Child : Parent
+    //{
+    //    public int Z { get; set; }
+
+    //    public Child(int x, int y, int z) : base(x, y)
+    //    {
+    //        Z = z;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return $"X={X}, Y={Y} ,Z={Z}";
+    //    }
+
+    //    public override int Product()
+    //    {
+    //        return X * Y * Z;
+    //    }
+
+    //    public new int Product()
+    //    {
+    //        return X * Y * Z;
+    //    }
+
+    //}
+
+
+    #endregion
+
+    #endregion
+
+}
