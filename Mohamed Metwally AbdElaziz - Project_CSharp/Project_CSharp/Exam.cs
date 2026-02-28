d211f295bc136d2f5f8f78677f7853c9a8bb1faa
Mohamed Metwally <metwallym527@gmail.com>
2026-02-28 23:41:19 +0200

    Add files via upload

---
+++ Mohamed Metwally AbdElaziz - Project_CSharp/Project_CSharp/Exam.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_CSharp
+{
+    enum ExamMode { Starting, Queued, Finished }
+
+    #region Exam class
+    internal abstract class Exam : ICloneable, IComparable<Exam>
+    {
+        public int Time { get; set; }
+
+        public Subject subject { get; set; }
+        public int NumberOfQuetion { get; set; }
+        public Dictionary<Quetion, Answer> correcttAnswer { get; set; }
+        public ExamMode mode { get; set; }
+        public QuetionList quetions { get; set; }
+
+        protected Exam(int _Time, int _NumberOfQuetion, Subject _subject)
+        {
+            Time = _Time;
+            NumberOfQuetion = _NumberOfQuetion;
+            correcttAnswer = new Dictionary<Quetion, Answer>();
+            quetions = new QuetionList("exam.txt");
+            mode = ExamMode.Queued;
+            subject = _subject;
+        }
+
+        public abstract void ShowExam();
+
+        public abstract object Clone();
+
+
+        public int CompareTo(Exam? other)
+        {
+            if (this.Time > other.Time)
+                return 1;
+            else if (this.Time < other.Time)
+                return -1;
+            else
+                return 0;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Exam: {subject.Name}, Quetions:{NumberOfQuetion}, Time:{Time} mins, mode:{mode}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Exam other)
+            {
+                return this.Time == other.Time &&
+                    this.NumberOfQuetion == other.NumberOfQuetion &&
+                    this.subject.Name == other.subject.Name;
+            }
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(NumberOfQuetion, subject.Name, Time);
+        }
+    }
+
+    #endregion
+
+    #region class Subject
+    class Subject
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+
+        public Subject(int _Code, string _Name)
+        {
+            Code = _Code;
+            Name = _Name;
+        }
+
+    }
+    #endregion
+
+    #region class PracticeExam
+    class PracticeExam : Exam
+    {
+
+        public PracticeExam(int _Time, int _NumberOfQuetion, Subject _subject) : base(_Time, _NumberOfQuetion, _subject)
+        {
+
+        }
+
+        public override object Clone()
+        {
+            return new PracticeExam(this.Time, this.NumberOfQuetion, this.subject);
+        }
+
+        public override void ShowExam()
+        {
+            int i = 1;
+
+            Console.WriteLine($"Practice Exam - Subject: {subject.Name} - Time: {Time} minutes");
+            Console.WriteLine();
+
+            foreach (var v in quetions)
+            {
+                v.ShowQuetion();
+
+
+                int choice;
+
+
+                do
+                {
+
+                    Console.WriteLine("Enter Correct Answer");
+
+                }
+                while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > v.Answers.Count);
+
+
+                Answer stuanswer = v.Answers[choice - 1];
+                Console.WriteLine($"Your answer: {stuanswer.Text}");
+
+                if (stuanswer.Text == correcttAnswer[v].Text)
+                    Console.WriteLine("Correct\n");
+                else
+                    Console.WriteLine($"Wrong! Correct answer is: {correcttAnswer[v].Text}\n");
+            }
+
+        }
+    }
+    #endregion
+
+    #region class FinalExam
+    class FinalExam : Exam
+    {
+        public FinalExam(int _Time, int _NumberOfQuetion, Subject _subject) : base(_Time, _NumberOfQuetion, _subject)
+        {
+
+        }
+
+        public override object Clone()
+        {
+            return new FinalExam(this.Time, this.NumberOfQuetion, this.subject);
+        }
+
+        public override void ShowExam()
+        {
+            Console.WriteLine($"Final Exam - Subject: {subject.Name} - Time: {Time} minutes");
+            Console.WriteLine();
+
+            int i = 1;
+
+            foreach (var v in quetions)
+            {
+                v.ShowQuetion();
+                Console.WriteLine();
+            }
+
+        }
+    }
+    #endregion
+}
---
+++ Mohamed Metwally AbdElaziz - Project_CSharp/Project_CSharp/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Project_CSharp;
+
+internal class Program
+{
+    static void Main()
+    {
+        Subject math = new Subject(747664, "Math");
+
+        PracticeExam practice = new PracticeExam(60, 10, math);
+
+        FinalExam final = new FinalExam(60, 10, math);
+
+        TrueOrFalseQuetion q1 = new TrueOrFalseQuetion("The Sun rises from the East", 5);
+        practice.quetions.Add(q1);
+        practice.correcttAnswer.Add(q1, q1.Answers[0]);
+
+        final.quetions.Add(q1);
+        final.correcttAnswer.Add(q1, q1.Answers[0]);
+
+        ChoiseOneQuetion q2 = new ChoiseOneQuetion("Which planet is known as the Red Planet?", 5);
+        q2.Answers.Add(new Answer("Earth"));
+        q2.Answers.Add(new Answer("Mars"));
+        q2.Answers.Add(new Answer("Jupiter"));
+        q2.Answers.Add(new Answer("Venus"));
+        practice.quetions.Add(q2);
+        practice.correcttAnswer.Add(q2, q2.Answers[1]);
+
+        final.quetions.Add(q2);
+        final.correcttAnswer.Add(q2, q2.Answers[1]);
+
+        ChoiseAllQuetion q3 = new ChoiseAllQuetion("Select prime numbers:", 5);
+        q3.Answers.Add(new Answer("2"));
+        q3.Answers.Add(new Answer("6"));
+        q3.Answers.Add(new Answer("4"));
+        q3.Answers.Add(new Answer("8"));
+        practice.quetions.Add(q3);
+        practice.correcttAnswer.Add(q3, q3.Answers[0]);
+
+        final.quetions.Add(q3);
+        final.correcttAnswer.Add(q3, q3.Answers[0]);
+
+
+        int input;
+        do
+        {
+            Console.Write("Select Exam Type: 1.Practice   2.Final :");
+
+        }
+        while (!int.TryParse(Console.ReadLine(), out input) || input < 1 || input > 2);
+
+        if (input == 1)
+        {
+            Console.WriteLine($"--- Practice Exam: {practice.subject.Name} ---\n");
+            practice.ShowExam();
+        }
+        else if (input == 2)
+        {
+            Console.WriteLine($"--- Final Exam: {final.subject.Name} ---\n");
+            final.ShowExam();
+        }
+
+    }
+
+}
