4a375c4aae1971538bc3c358955ad8e99179949e
Mohamed Metwally <metwallym527@gmail.com>
2026-04-15 02:03:58 +0200

    Delete OneDrive - October 6 University Egypt/Desktop directory

--- OneDrive - October 6 University Egypt/Desktop/ASP.NET04/ASP.NET04/Demo/ASP.Net02/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.Net02.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
